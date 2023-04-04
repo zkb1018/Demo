f40b6f7f6ead2348544d8d2753ebd4bc1c3fce7a
zhangkaibin <1453335882@qq.com>
2023-04-04 11:19:40 +0800

    Initial Commit

---
+++ Demo/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Demo
+//
+//  Created by 张开斌 on 2023/4/4.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
